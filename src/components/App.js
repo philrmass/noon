ea9b0420b6065aa8d5ff5069ab7513e1116db932
Phil Mass <philrmass@gmail.com>
2021-10-25 21:58:11 -0700

    add redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,18 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import { getWeather } from '../redux/weatherActions';
 import styles from './App.module.css';
 
 export default function App() {
+  const dis = useDispatch();
+  const weather = useSelector(state => state.weather.forecast);
+
   return (
-    <div className={styles.app}>APP</div>
+    <div className={styles.app}>
+      <button onClick={() => dis(getWeather())}>
+        Get Weather
+      </button>
+      <div>{`Weather: ${weather}`}</div>
+    </div>
   );
 }
