22364f24ac7016ab5987137e20053d7f134fded7
Phil Mass <philrmass@gmail.com>
2021-10-30 11:17:33 -0700

    add marker to map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import L from 'leaflet';
 
 import { setupMap } from '../utilities/map';
 import styles from './Map.module.css';
@@ -8,25 +9,51 @@ import styles from './Map.module.css';
 export default function Map() {
   const id = 'mapId';
   const [text, setText] = useState('');
+  const [map, setMap] = useState();
 
   useEffect(() => {
-    const map = setupMap(id);
-    if ('geolocation' in navigator) {
-      setText('geo is ok');
-      navigator.geolocation.getCurrentPosition((position) => {
-        setText(`geo is [${position.coords.latitude} ${position.coords.longitude}]`);
-        const point = [position.coords.latitude, position.coords.longitude];
-        map.setView(point, 13, { animation: true });        
-      });
-    } else {
-      setText('geo NOT ok');
-    }
-    console.log('MAP', map);
+    setMap(setupMap(id));
   }, []);
+
+  const locate = () => {
+    /*
+    const pt = [45, -122];
+    map.setView(pt);
+    //const marker = L.marker(pt).addTo(map);
+    var circle = L.circle(pt, {
+      color: 'red',
+      fillColor: '#f03',
+      fillOpacity: 0.5,
+      radius: 5,
+    }).addTo(map);
+    setText(`Locate to [${pt[0]} ${pt[1]}] (${typeof circle})`);
+    */
+
+    if (map && navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((pos) => {
+        const pt = [pos.coords.latitude, pos.coords.longitude];
+
+        map.setView(pt);
+        var marker = L.circle(pt, {
+          color: 'red',
+          fillColor: '#f03',
+          fillOpacity: 0.5,
+          radius: 5,
+        }).addTo(map);
+        //const marker = L.marker(pt).addTo(map);
+        setText(`Locate to [${pt[0]} ${pt[1]}] (${typeof marker})`);
+      });
+    }
+  };
 
   return (
     <>
-      <div className={styles.text}>{`[${text}]`}</div>
+      <div className={styles.controls}>
+        <button className='button' onClick={locate}>
+          Locate
+        </button>
+        <div className={styles.text}>{`[${text}]`}</div>
+      </div>
       <div id={id} className={styles.map}></div>
     </>
   );
